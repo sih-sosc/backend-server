3460f7ca9529a3df0d228e2baf1278e057e49b7b
unknown <akashdeepb.cs17@sahyadri.edu.in>
2019-02-23 20:59:12 +0530

    Registration Implemented

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,3 +5,10 @@ exports._sign_in_checks = [
     check('email').isEmail().exists(),
     check('password').isLength({min: 6}).exists()
 ];
+
+exports._register_checks = [
+    check('email').isEmail().exists(),
+    check('password').isLength({min: 6}).exists(),
+    check('full_name').isAlpha().exists(),
+    check('phone_number').isNumeric().isLength({min: 9}).exists()
+];
--- routes/register.js
+++ routes/register.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const user = require('../models/user');
 const router = express.Router();
+const AuthController = require('../controllers/AuthController');
 
 /* POST Request for Registration    */
-router.post('/', function(req, res){
+router.post('/', AuthController._register_checks, function(req, res){
     let newUser = new user(req.body);
     newUser.save((err, usr) => {
         if(err)
-            res.status(500).send('Something Went Wrong');
+            res.status(500).send(err);
         else
             res.status(200).json(usr);
     });
