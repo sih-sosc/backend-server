06eb0e647442896fbb591f27500f13f90ad05bce
unknown <akashdeepb.cs17@sahyadri.edu.in>
2019-02-23 21:10:14 +0530

    Added Password to User Model

--- models/user.js
+++ models/user.js
@@ -8,6 +8,10 @@ const userSchema = new Schema ({
         required: true,
         unique: true
     },
+    password : {
+        type: String,
+        required: true
+    },
     full_name : {
         type: String,
         required: true
