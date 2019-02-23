b286cc96e29e9f848c2bfe990dfaf9c9f1554a9c
unknown <akashdeepb.cs17@sahyadri.edu.in>
2019-02-23 20:34:32 +0530

    Implemented Register

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-//app.use('/register', registerRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
