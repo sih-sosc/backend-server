9d890e30884e78fbc4dadfd9003fd0de25081799
unknown <akashdeepb.cs17@sahyadri.edu.in>
2019-02-23 20:43:06 +0530

    Minor fix in Register Module

--- routes/register.js
+++ routes/register.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* POST Request for Registration    */
 router.post('/', function(req, res){
     let newUser = new user(req.body);
-    user.save((err, usr) => {
+    newUser.save((err, usr) => {
         if(err)
             res.status(500).send('Something Went Wrong');
         else
