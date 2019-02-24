e8b9802f6c5af889ac735066d2514d5378dcbb31
unknown <akashdeepb.cs17@sahyadri.edu.in>
2019-02-24 08:45:06 +0530

    Implemented Log Viewing Basic Functionality

--- routes/view_logs.js
+++ routes/view_logs.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 const user = require('../models/user');
 
+/*   GET REQUEST TO VIEW LOGS    */
 router.get('/', AuthController.verify_token, function(req, res, next){
+
+    //Find Email in Database
     user.find({email: req.decoded.email}, (err, _req) => {
         if(err)
             res.status(500).send(err);
         else{
-            user.find({email: req.decoded.email, permission:{view_logs: true }},(err, _req) => {
+
+            //Check if the User has Permission to view Logs
+            user.find({email: req.decoded.email, 'permissions.manage_logs': true },(err, _req) => {
                 if(err)
-                    res.status(500).send("403 - Forbidden");
+                    res.status(500).send(err);
+                else if (_req.length == 0)
+                    res.status(403).send('403 - FORBIDDEN');
                 else
                     res.status(200).json(_req);
             });
